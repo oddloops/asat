e2434d335f854f01648b6c5cfcc7d26612ba30e8
Vinci Wu <wu.vinci00@gmail.com>
2023-10-16 17:02:49 -0400

    Restored App files

--- frontend/anomaly-detection/src/index.js
+++ frontend/anomaly-detection/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { initializeApp } from 'firebase/app';
 // import { getAnalytics } from "firebase/analytics";
@@ -20,6 +21,7 @@ const app = initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
 
