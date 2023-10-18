5049b5fa73027d911191ea48552ac426ab5dbd6a
Vinci Wu <wu.vinci00@gmail.com>
2023-10-17 23:10:01 -0400

    Created routes to profile page, login page, and register page

--- frontend/anomaly-detection/src/App.js
+++ frontend/anomaly-detection/src/App.js
@@ -1,9 +1,49 @@
 import './App.css';
+import { Navigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Authentication/Register';
+import Login from './components/Authentication/Login';
+import VerifyEmail from './components/Authentication/VerifyEmail';
+import Profile from './components/Profile/Profile';
+import PrivateRoute from './PrivateRoute';
+import { useState, useEffect } from 'react';
+import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { AuthProvider } from './AuthContext';
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+  const [timeActive, setTimeActive] = useState(false)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+  }, [])
+
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
+        <Routes>
+          <Route exact path='/' element={
+            <PrivateRoute>
+              <Profile/>
+            </PrivateRoute>
+          }/>
+          <Route path="/login" element={
+            !currentUser?.emailVerified 
+            ? <Login/>
+            : <Navigate to='/' replace/>
+          } />
+          <Route path="/register" element={
+            !currentUser?.emailVerified 
+            ? <Register/>
+            : <Navigate to='/' replace/>
+          } />
+          <Route path='/verify-email' element={<VerifyEmail/>} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
 }
 
