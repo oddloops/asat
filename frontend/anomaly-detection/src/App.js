48ff95bbf7c4ccfd3718522b33b0e259543e943b
Vinci Wu <wu.vinci00@gmail.com>
2023-10-19 00:02:54 -0400

    Removed login and register pages and added ImageUpload component

--- frontend/anomaly-detection/src/App.js
+++ frontend/anomaly-detection/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { Navigate, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Register from './components/Authentication/Register';
-import Login from './components/Authentication/Login';
-import VerifyEmail from './components/Authentication/VerifyEmail';
-import Profile from './components/Profile/Profile';
-import PrivateRoute from './PrivateRoute';
+// import Register from './components/Authentication/Register';
+// import Login from './components/Authentication/Login';
+// import VerifyEmail from './components/Authentication/VerifyEmail';
+// import Profile from './components/Profile/Profile';
+// import PrivateRoute from './PrivateRoute';
 import { useState, useEffect } from 'react';
 import { auth } from './firebase';
 import { onAuthStateChanged } from 'firebase/auth';
 import { AuthProvider } from './AuthContext';
+import ImageUploadForm from './components/ImageUploadForm/ImageUploadForm';
 
 function App() {
 
@@ -22,29 +23,30 @@ function App() {
   }, [])
 
   return (
-    <Router>
-      <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
-        <Routes>
-          <Route exact path='/' element={
-            <PrivateRoute>
-              <Profile/>
-            </PrivateRoute>
-          }/>
-          <Route path="/login" element={
-            !currentUser?.emailVerified 
-            ? <Login/>
-            : <Navigate to='/' replace/>
-          } />
-          <Route path="/register" element={
-            !currentUser?.emailVerified 
-            ? <Register/>
-            : <Navigate to='/' replace/>
-          } />
-          <Route path='/verify-email' element={<VerifyEmail/>} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
+    ImageUploadForm()
+  )
+  // return (
+  //   <Router>
+  //     <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
+  //       <Routes>
+  //         <Route exact path='/' element={
+  //           ImageUploadForm
+  //         }/>
+  //         <Route path="/login" element={
+  //           !currentUser?.emailVerified 
+  //           ? <Login/>
+  //           : <Navigate to='/' replace/>
+  //         } />
+  //         <Route path="/register" element={
+  //           !currentUser?.emailVerified 
+  //           ? <Register/>
+  //           : <Navigate to='/' replace/>
+  //         } />
+  //         <Route path='/verify-email' element={<VerifyEmail/>} />
+  //       </Routes>
+  //     </AuthProvider>
+  //   </Router>
+  // );
 }
 
 export default App;
