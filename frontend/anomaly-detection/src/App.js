cf4c9e36b928f471d408ef56a160721a66f8adca
Vinci Wu <wu.vinci00@gmail.com>
2023-11-16 13:58:35 -0500

    Removed firebase auth commented code to improve readability

--- frontend/anomaly-detection/src/App.js
+++ frontend/anomaly-detection/src/App.js
@@ -5,52 +5,24 @@ import { Navigate, BrowserRouter as Router, Routes, Route } from 'react-router-d
 // import VerifyEmail from './components/Authentication/VerifyEmail';
 // import Profile from './components/Profile/Profile';
 // import PrivateRoute from './PrivateRoute';
-import { useState, useEffect } from 'react';
-import { auth } from './firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { AuthProvider } from './AuthContext';
-import ImageUploadForm from './components/ImageUploadForm/ImageUploadForm';
-import ImageDropzone from './components/ImageUploadForm/ImageDropzone';
+// import { useState, useEffect } from 'react';
+// import { auth } from './firebase';
+// import { onAuthStateChanged } from 'firebase/auth';
+// import { AuthProvider } from './AuthContext';
+// import ImageUploadForm from './components/ImageUploadForm/ImageUploadForm';
+import ImageUploadPage from './components/ImageUploadForm/ImageUploadPage';
 
 function App() {
-
-  const [currentUser, setCurrentUser] = useState(null)
-  const [timeActive, setTimeActive] = useState(false)
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user)
-    })
-  }, [])
-
   return (
     <>
-      <h1>AAsat</h1>
-      {ImageDropzone()}
+      <h1>AAsat Color Palette Extraction</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ImageUploadPage/>}/>
+        </Routes>
+      </Router>
     </>
   )
-  // return (
-  //   <Router>
-  //     <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
-  //       <Routes>
-  //         <Route exact path='/' element={
-  //           ImageUploadForm
-  //         }/>
-  //         <Route path="/login" element={
-  //           !currentUser?.emailVerified 
-  //           ? <Login/>
-  //           : <Navigate to='/' replace/>
-  //         } />
-  //         <Route path="/register" element={
-  //           !currentUser?.emailVerified 
-  //           ? <Register/>
-  //           : <Navigate to='/' replace/>
-  //         } />
-  //         <Route path='/verify-email' element={<VerifyEmail/>} />
-  //       </Routes>
-  //     </AuthProvider>
-  //   </Router>
-  // );
 }
 
 export default App;
