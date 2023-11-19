6e0aa96c8e880d14e4403869774c9847124265c9
Vinci Wu <wu.vinci00@gmail.com>
2023-11-19 12:54:44 -0500

    Fixed some errors in django backend

--- backend/color_extraction/color_app/models.py
+++ backend/color_extraction/color_app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class ColorExtraction(models.Model):
-    image = models.ImageField(upload_to='/uploads')
+    image = models.ImageField(upload_to='uploads')
     extracted_colors = models.JSONField(default=list)
