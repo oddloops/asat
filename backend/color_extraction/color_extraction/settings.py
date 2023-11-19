976e729433e1a208014e9b948f2ef68431953697
Vinci Wu <wu.vinci00@gmail.com>
2023-11-19 15:58:47 -0500

    Removed dummy database to use sqlite

--- backend/color_extraction/color_extraction/settings.py
+++ backend/color_extraction/color_extraction/settings.py
@@ -84,7 +84,8 @@ WSGI_APPLICATION = 'color_extraction.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
